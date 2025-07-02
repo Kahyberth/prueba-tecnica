a7bed8ddeaa3bfbdc3ece8bb4ce11ff05a8305fc
Kahyberth <kahyberth@gmail.com>
2025-07-02 06:47:24 -0500

    fix(dto): trim and strengthen validations for CreateUserDto fields

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,17 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsEmail()
+  @MinLength(1)
+  @Transform(({ value }) => value.trim())
   @IsNotEmpty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(1)
+  @Transform(({ value }) => value.trim())
   name: string;
 }
